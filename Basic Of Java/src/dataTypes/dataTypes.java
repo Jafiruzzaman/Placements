41782e2f08e0dcb54e80106ad43b589d28e42156
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-02 20:36:18 +0600

    datatypes in java

---
+++ Basic Of Java/src/dataTypes/dataTypes.java
@@ -0,0 +1,63 @@
+package dataTypes;
+
+public class dataTypes {
+    public static void main(String[] args) {
+        System.out.println("datatype in java");
+    /*
+        There are 2 types of datatype in java
+        1. primitive datatype
+            1. byte datatype
+                byte size is 1 byte. 1 byte is 8 bit. default value is 0
+            2. short datatype
+                short size is 2 byte. default value is 0
+            3. int datatype
+                int size is 4 byte. default value is 0
+            4. float datatype
+                float size is 4 byte. default value is 0.0f
+            5. double datatype
+                double size is 8 byte. default value is 0.0d
+            6. boolean datatype
+                boolean size it depends on JVM and on your OS
+            7. char datatype
+                char size is 1 byte. default value is \U000
+            8. long datatype
+                long size is 8 byte. default value is 0L
+        2. non-primitive
+            1. Array
+            2. String
+            3. Functions/methods
+    */
+//        1. byte datatype
+        byte miniNumber = 127;
+        System.out.println(miniNumber);
+
+//        2. short datatype
+        short shortNumber = 31240;
+        System.out.println(shortNumber);
+
+//        3. int datatype
+        int number = 4125;
+        System.out.println(number);
+
+//        4. float datatype
+        float height = 5.5f;
+        System.out.println(height);
+
+//        5. double datatype
+        double weight = 41.5;
+        System.out.println(weight);
+
+//        6. boolean datatype
+        boolean isAuthenticate = false;
+        System.out.println(false);
+
+//        7. char datatype
+        char c = 'c';
+        System.out.println(c);
+
+//        8. long datatype
+        long bigBang = 41345241254155142L;
+        System.out.println(bigBang);
+
+    }
+}
