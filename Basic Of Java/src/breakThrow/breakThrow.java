30bd7934044174e60d5106069dbe7722dbf1c9ac
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-02 20:09:10 +0600

    java program breakThrow

---
+++ Basic Of Java/src/breakThrow/breakThrow.java
@@ -0,0 +1,23 @@
+package breakThrow;
+
+public class breakThrow {
+    public static void main(String[] args) {
+        System.out.println("break Throw of java program");
+    }
+}
+// This is single line comment
+/*
+This is multiline comment
+ */
+/*
+    package is a collection of classes, sub packages and object's
+    public --> public is an access modifier
+    class --> class is a user defined blueprint
+    breakThrow --> breakThrow is the name of public class
+    static --> static allow us to call methods without creating classes
+    void --> void is the return type. void means null
+    main --> main is the name of function. in java at when a java program start it runs the main functions start
+    (String[] args) --> command line argument's
+    System --> System is a class include multiple methods and objects and Standard input and out stream
+    println --> println allows to print in a new line
+ */
