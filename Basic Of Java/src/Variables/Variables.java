15b1238864c8a199abeb51705a0d951247b1519a
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-02 20:15:52 +0600

    Variables Concept's

---
+++ Basic Of Java/src/Variables/Variables.java
@@ -0,0 +1,23 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Variables in java");
+        int num = 20;
+        System.out.println(num);
+    }
+}
+
+/*
+Variables are container to store a value or another way variables are the title of memory locations
+
+    int num = 20;
+    here int is the datatype , num is the variable name, and 20 is the variable assign value
+    Rules while creating variables
+    1. variables name can't start with number's or any other special character's
+    2. variables name are case-sensitive
+    3. variables name must start with letter , dollar sign ($) and underScore (_)
+    4. variables name can't be a keyword
+    5. variables name should be meaningful
+
+ */
